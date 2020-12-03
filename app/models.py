e37cfa71c6579880676b6b35e6febab9b1b03b75
MuffinMauler <NathanRobertson@dal.ca>
2020-12-03 11:41:38 -0400

    Added jokes model corresponding to the jokes table in the db

--- app/models.py
+++ app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class joke(models.Model):
+    setup = models.CharField(max_length=255)
+    delivery = models.CharField(max_length=255)
+
+    class Meta:
+        #name the table jokes
+        db_table = 'jokes'

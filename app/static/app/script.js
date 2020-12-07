3251940416164395789f1182e7ecad9d9d0a7035
MuffinMauler <NathanRobertson@dal.ca>
2020-12-07 10:38:52 -0400

    Fleshed out and added various JS functions

--- app/static/app/script.js
+++ app/static/app/script.js
@@ -1,5 +1,4 @@
-console.log('It is working!');
-
+initializeChat();
 /*
 	Notes for the morning:
 
@@ -15,7 +14,62 @@ console.log('It is working!');
 	leads to: everything else (initial state and return state)
 */
 function initializeChat() {
+	console.log('Initialize chat');
 
+
+	/* onload came from the second answer here: https://stackoverflow.com/questions/26107125/cannot-read-property-addeventlistener-of-null */
+	window.onload = function(){
+        document.getElementById("send").addEventListener("click", addUserMessage, false);
+        document.getElementById("knock").addEventListener("click", knockKnock, false);
+
+		addBotMessage('Hi there! My name is Jokebot and I love knock-knock jokes.');
+    }
+
+}
+
+/*
+	Add a message on the user side. Text is pulled from the text area and cleared
+*/
+function addUserMessage(messageToAdd) {
+	/* reference appropriate elements */
+	editText = document.querySelector(".edit-text");
+
+	if (typeof messageToAdd != "string") { /* if message is coming from text box */
+		/* return if no text to add */
+		if (editText.value === '') {
+			return;
+		}
+		messageToAdd = editText.value;
+	}
+	messages = document.querySelector(".messages");
+
+	messageDiv = document.createElement("div");
+	messageDiv.appendChild(document.createTextNode(messageToAdd));
+
+	/* add appropriate classes */
+	messageDiv.classList.add("message");
+	messageDiv.classList.add("user-message");
+
+	editText.value = ''; /* clear text box */
+	messages.appendChild(messageDiv); /* append message to messages */
+}
+
+
+/*
+	Add a message on the bot side. This will only be triggered by user interaction
+*/
+function addBotMessage(messageToAdd) {
+	/* reference message list */
+	messages = document.querySelector(".messages");
+
+	messageDiv = document.createElement("div");
+	messageDiv.appendChild(document.createTextNode(messageToAdd));
+
+	/* add appropriate classes */
+	messageDiv.classList.add("message");
+	messageDiv.classList.add("bot-message");
+
+	messages.appendChild(messageDiv); /* append message to messages */
 }
 
 /*
@@ -24,7 +78,14 @@ function initializeChat() {
 	leads to: tellSetup()
 */
 function knockKnock() {
+	addUserMessage('Knock knock');
+	setTimeout( function() {
+		addBotMessage('Who\'s there?');
+	}, 1000 )
 
+	/* change the function that the send button is connected to */
+	document.getElementById("send").removeEventListener("click", addUserMessage, false);
+	document.getElementById("send").addEventListener("click", tellSetup, false);
 }
 
 /*
@@ -51,7 +112,21 @@ function fetchJoke() {
 	leads to: tellPunchline(), forgotJoke() (cancel), 
 */
 function tellSetup() {
+	editText = document.querySelector(".edit-text");
+	setup = editText.value;
 
+	/* TODO: add forgotJoke preset */
+	addUserMessage(setup); /* add message to chat */
+	setTimeout( function() { /* add bot message to chat */
+		addBotMessage(setup + '  who?');
+	}, 1000 )
+
+	document.getElementById("send").removeEventListener("click", tellSetup, false);
+	/* passing callback functions with arguments: https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener */
+	document.getElementById("send").addEventListener("click", function completeJoke(){
+		tellPunchline(setup);
+		document.getElementById("send").removeEventListener("click", completeJoke, false);
+	}, false);
 }
 
 /*	
@@ -60,7 +135,7 @@ function tellSetup() {
 	leads to: return state
 */
 function forgotJoke() {
-
+	document.getElementById("send").addEventListener("click", addUserMessage, false);
 }
 
 /*
@@ -74,11 +149,39 @@ function hearSetup() {
 
 /*
 	Complete the joke which jokebot then adds to the DB (if possible)
-	dependencies: tellPunchline(), access to keyboard
+	dependencies: access to keyboard
 	leads to: addJoke(), return state
 */
-function tellPunchline() {
+function tellPunchline(setup) {
+	console.log("Setup: " + setup);
 
+	delivery = editText.value;
+	addUserMessage(delivery);
+
+	joke = {
+		"setup": setup,
+		"delivery": delivery
+	}
+
+	setTimeout( function() {
+		addBotMessage("That is a good one. I will have to remember it.");
+	}, 1000 )
+
+	document.getElementById("send").addEventListener("click", addUserMessage, false);
+
+	/* TODO: add joke to DB */
+	console.log(joke);
+}
+
+function completeJoke(setup, delivery) {
+	document.getElementById("send").addEventListener("click", function(){
+		joke = {
+			"setup": setup,
+			"delivery": delivery
+		}
+	}, false );
+	console.log(joke);
+	/* TODO submit joke */
 }
 
 /*
@@ -105,4 +208,20 @@ function addJoke() {
 
 function deleteJoke() {
 
+}
+
+function addPreset() {
+
+}
+
+function deletePreset() {
+
+}
+
+function printList(jokes) {
+	parsed = JSON.parse(jokes)
+
+	for (const joke in parsed) {
+		console.log(joke)
+	}
 }

04098063836ee2e8ab782d41654219a1f04a8cea
MuffinMauler <NathanRobertson@dal.ca>
2020-12-07 08:06:10 -0400

    Added some get methods to models.py

--- app/models.py
+++ app/models.py
@@ -4,6 +4,15 @@ class joke(models.Model):
     setup = models.CharField(max_length=255)
     delivery = models.CharField(max_length=255)
 
+    def get_setup(self):
+        return self.setup
+
+    def get_delivery(self):
+        return self.delivery
+
+    def __str__(self):
+        return "Setup: " + self.setup + "\nDelivery: " + self.delivery
+
     class Meta:
         #set table name as jokes for clarity
         db_table = 'jokes'
